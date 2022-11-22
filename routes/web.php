0e0a7a4f39158504bf94bf227416085b49daa2d2
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2022-11-22 11:33:35 +0600

    schema fix

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/test', function () {
+    return view('test');
+});
